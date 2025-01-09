1fb2a85099d8c118442314cd535ec11a39f60b25
Kanzaki Taiki <w.j.81.best8@gmail.com>
2025-01-09 14:29:27 +0000

    HHKB過去問実施

---
+++ atCoder/Beginner282/A/Main.java
@@ -0,0 +1,22 @@
+package atCoder.Beginner282.A;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		// 値を受け取る
+		int k = sc.nextInt();
+		sc.close();
+
+		// アルファベットを格納する配列を作ってしまう。
+		String[]  alphabet = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+
+		// 受け取った数だけループして文字列を作成して出力する。
+		String ans = "";
+		for (int i = 0; i < k; i++) {
+			ans = ans.concat(alphabet[i]);
+		}
+		System.out.println(ans);
+    }
+}
